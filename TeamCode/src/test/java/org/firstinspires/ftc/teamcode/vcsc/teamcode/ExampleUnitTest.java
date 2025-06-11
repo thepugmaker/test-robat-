6e996e9a06d5a40b472e30408536e6d88d6a3e80
thepugmaker <93961336+thepugmaker@users.noreply.github.com>
2025-06-11 13:32:35 -0700

    Initial commit

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/vcsc/teamcode/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package vcsc.teamcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleUnitTest {
+  @Test
+  public void addition_isCorrect() {
+    assertEquals(4, 2 + 2);
+  }
+}
